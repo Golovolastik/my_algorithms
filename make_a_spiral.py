ef98a605d8096a62c16872f8e3d095f8bcfce6ac
David Lynch <yankovich.aliaksei@gmail.com>
2021-03-08 12:25:18 +0300

    come up with algorithm

---
+++ make_a_spiral.py
@@ -0,0 +1,22 @@
+# Your task, is to create a NxN spiral with a given size.
+
+import numpy as np
+
+def spiralsize(size):
+	import numpy as np
+	array = np.zeros((size, size), dtype=int)
+	array[0] = 1
+	array[:, -1] = 1
+	array[-1] = 1
+	array[0+2:, 0] = 1
+	array[0+2, :-1-1] = 1
+	array[0+2:-2, -1-2] = 1
+	return array
+
+print(spiralsize(6))
+
+#array = np.random.randint(size=(5, 5), low=0, high=9)
+#print(array)
+#print(array[0])
+#print(array[0][0])
+#print(array[:, -1])

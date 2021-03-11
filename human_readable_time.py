4f7a5a95d114d18d8e5226798c8b02d18a62cf0a
David Lynch <yankovich.aliaksei@gmail.com>
2021-03-11 16:13:36 +0300

    removed files from other branches

--- human_readable_time.py
+++
@@ -1,9 +0,0 @@
-seconds = 5
-
-def make_readable(seconds):
-    HH = seconds // 3600
-    MM = (seconds - HH * 3600) // 60
-    SS = seconds - HH * 3600 - MM * 60
-    return f"{str(HH).zfill(2)}:{str(MM).zfill(2)}:{str(SS).zfill(2)}"
-
-print(make_readable(seconds))
--- palindrome_creator.py
+++
@@ -1,9 +0,0 @@
-string = "blabla"
-
-def create_palindrome(str):
-    n = 0
-    while str[n:] != str[n:][::-1]:
-    	n += 1
-    return str + str[:n][::-1]
-
-print(create_palindrome(string))

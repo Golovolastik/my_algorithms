4f17e65dff981d1b32be8e17818a8b7707a535a6
David Lynch <yankovich.aliaksei@gmail.com>
2021-03-14 16:13:49 +0300

    difficult version (over 9000)

--- get_pins.py
+++ get_pins.py
@@ -8,49 +8,96 @@
 # that returns an array of all variations for an observed PIN
 # with a length of 1 to 8 digits.
 
-import numpy as np
-keypad = np.array([[1, 2, 3],
-                   [4, 5, 6],
-                   [7, 8, 9],
-                   [np.NaN, 0, np.NaN]])
-
 def get_pins(observed):
+    import numpy as np
+    keypad = np.array([[1, 2, 3],
+                       [4, 5, 6],
+                       [7, 8, 9],
+                  [np.NaN, 0, np.NaN]])
     expectations = []
     possible_numbers = {}
-    for pos, number in enumerate(observed):
-        possible_numbers[pos] = []
-        result = np.where(keypad == int(number))
-        x, y = result[-1][0], result[0][0]
-        possible_numbers[pos].append((int(number)))
-        if x < 2:
-            try:
-                number1 = keypad[y][x+1]
-                possible_numbers[pos].append((int(number1)))
-            except:
-                pass
-        if x > 0:
-            try:
-                number2 = keypad[y][x-1]
-                possible_numbers[pos].append((int(number2)))
-            except:
-                pass
-        if y < 3:
-            try:
-                number3 = keypad[y+1][x]
-                possible_numbers[pos].append((int(number3)))
-            except:
-                pass
-        if y > 0:
-            try:
-                number4 = keypad[y-1][x]
-                possible_numbers[pos].append((int(number4)))
-            except:
-                pass
-    arr = [possible_numbers[idx] for idx in range(len(possible_numbers))]
-    combined_array = np.array(np.meshgrid(arr)).T.reshape(-1, len(arr))
+    def find_possible_numbers():
+        for pos, number in enumerate(observed):
+            possible_numbers[pos] = []
+            result = np.where(keypad == int(number))
+            x, y = result[-1][0], result[0][0]
+            possible_numbers[pos].append((int(number)))
+            if x < 2:
+                try:
+                    number1 = keypad[y][x+1]
+                    possible_numbers[pos].append((int(number1)))
+                except:
+                    pass
+            if x > 0:
+                try:
+                    number2 = keypad[y][x-1]
+                    possible_numbers[pos].append((int(number2)))
+                except:
+                    pass
+            if y < 3:
+                try:
+                    number3 = keypad[y+1][x]
+                    possible_numbers[pos].append((int(number3)))
+                except:
+                    pass
+            if y > 0:
+                try:
+                    number4 = keypad[y-1][x]
+                    possible_numbers[pos].append((int(number4)))
+                except:
+                    pass
+        arr = [possible_numbers[idx] for idx in range(len(possible_numbers))]
+        return arr
+    def combinate(arr):
+        n = len(arr)
+        combinations = []
 
-    return 'hello' 
+        # to keep track of next element 
+        # in each of the n arrays
+        indices = [0 for i in range(n)]
+        
+        while (1):
+            combination = []
 
-PIN = '87'
-print(keypad)
+            # prcurrent combination
+            for i in range(n):
+                combination.append(arr[i][indices[i]])
+            combinations.append(combination)
+
+            # find the rightmost array that has more
+            # elements left after the current element
+            # in that array
+            next = n - 1
+            while (next >= 0 and
+                (indices[next] + 1 >= len(arr[next]))):
+                next-=1
+
+            # no such array is found so no more
+            # combinations left
+            if (next < 0):
+                return combinations
+
+            # if found move to next element in that
+            # array
+            indices[next] += 1
+
+            # for all arrays to the right of this
+            # array current index again points to
+            # first element
+            for i in range(next + 1, n):
+                indices[i] = 0
+    def create_string(arr):
+        result = []
+        for combo in arr:
+            combination = ""
+            for number in combo:
+                combination += str(number)
+            result.append(combination)
+        return result
+    array = find_possible_numbers()
+    array = combinate(array)
+    array = sorted(create_string(array))
+    return array
+
+PIN = '07'
 print(get_pins(PIN))

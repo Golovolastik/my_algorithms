de13917b00edf842cd104bc2112ae7e668eb20db
Golovolastik <72813121+Golovolastik@users.noreply.github.com>
2021-12-22 14:05:25 +0300

    vector omg

--- с++/gravity/main.cpp
+++ с++/gravity/main.cpp
@@ -1,7 +1,14 @@
 // description is in the 'descr.h' file
+#include <vector>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+std::vector<int> flip;
+
+int main(){
+    std::vector<int>&before;
     return 0;
+};
+
+std::vector<int> flip(const char dir, const std::vector<int>& arr) {
+    return arr;
 }

3bcbd22cc690b6fd5a0c513cc14e80436291be4f
Golovolastik <72813121+Golovolastik@users.noreply.github.com>
2022-02-03 13:22:59 +0300

    del add

--- python_workout/exercise_6.py
+++ python_workout/exercise_6.py
@@ -10,18 +10,3 @@ def pig_latin_sentence(sent):
 	return " ".join(output)
 
 print(pig_latin_sentence('this is a test translation'))
-
-#additional exercise
-def bla_bla(list):
-	print(len(list))
-	temp = []
-	output = [[]]*len(list)
-	print(output)
-	for string in list:
-		temp.append(string.split())
-		temp
-	for i, string in enumerate(list):
-		output[i].append(temp[i])
-	print(output)
-
-print(bla_bla(['abc def ghi','jkl mno pqr','stu vwx yz']))
